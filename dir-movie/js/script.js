cb07519803108f7bad7791234a7f0fd3d8ba2b02
Mister K <khidirgaminghd@gmail.com>
2026-06-03 17:59:22 +0700

    Feat: Menambahkan Fitur modal toggle buat melihat detail

--- dir-movie/js/script.js
+++ dir-movie/js/script.js
@@ -1,5 +1,6 @@
-$('#search-button').on('click', function (){
-    $('#movie-list').html('');
+
+function searchMovie(){
+     $('#movie-list').html('');
     
     $.ajax({
         url: 'http://omdbapi.com',
@@ -20,7 +21,8 @@ $('#search-button').on('click', function (){
                                 <div class="card-body">
                                     <h5 class="card-title">`+data.Title+`</h5>
                                     <h6 class="card-subtitle mb-2 text-muted">`+data.Year+`</h6>
-                                   <a href="#" class="card-link">See Details</a>
+                                   <a href="#" class="card-link see-detail" data-toggle="modal" data-target="#exampleModal" data-id="`+data.imdbID+`">
+                                   See Details</a>
                                 </div>
                             </div>
                         </div>
@@ -38,5 +40,49 @@ $('#search-button').on('click', function (){
             }
         }
     });
+};
+
+$('#search-button').on('click', function (){
+    searchMovie();
 });
+
+$('#search-input').on('keyup', function (e){
+    if(e.keyCode === 13){
+        searchMovie();
+    }
+});
+
+$('#movie-list').on('click', '.see-detail', function(){
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type:'get',
+        dataType: 'json',
+        data:{
+            'apikey': '9976bbe1',
+            'i':$(this).data('id')
+        },
+        success: function(movie){
+            if(movie.Response === "True"){
+                $('.modal-body').html(`
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src ="` +movie.Poster+`" class="img-fluid">
+                        </div>
+                        <div class="col-md-8">
+                            <ul class="list-group">
+                                <li class="list-group-item"><h3>Title: `+movie.Title+`</h3></li>
+                                <li class="list-group-item">Release Date: `+movie.Released+`</li>
+                                <li class="list-group-item">Genre: `+movie.Genre+`</li>
+                                <li class="list-group-item">Director: `+movie.Director+`</li>
+                                <li class="list-group-item">Actors: `+movie.Actors+`</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                `);
+            }
+        }
+    })
+})
 
