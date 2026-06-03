11bd486b9144e5c164e330864a3536111260a563
Mister K <khidirgaminghd@gmail.com>
2026-06-03 16:56:36 +0700

    first commit

---
+++ json/latihan1.php
@@ -0,0 +1,19 @@
+<?php
+
+$mahasiswa = [
+    [
+        "nama" => "khidir ali",
+        "nis" => "08938483",
+        "email" => "khidirali@gmail.com"
+    ],
+    [
+        "nama" => "khidir ali",
+        "nis" => "08938483",
+        "email" => "khidirali@gmail.com"
+    ]
+];
+
+$data= json_encode($mahasiswa);
+echo $data;
+
+?>
